72f4e79323842740bf69369c4e1778a7326456fc
Oleg Tolochko <143202406+OlegAnTo2000@users.noreply.github.com>
2024-10-17 22:29:05 +0800

    Update field.blade.php

--- views/field.blade.php
+++ views/field.blade.php
@@ -1,6 +1,11 @@
 @component($typeForm, get_defined_vars())
-    <div data-controller="tinymce" data-theme="{{$theme ?? 'silver'}}" data-language="{{$language ?? 'en'}}"
-         @if ($configExt) data-config-ext="{{ $configExt}}" @endif>
+    <div data-controller="tinymce" 
+        data-theme="{{$theme ?? 'silver'}}" 
+        data-language="{{$language ?? 'en'}}"
+        @if ($storage) data-storage="{{$storage ?? 'public'}}" @endif
+        @if ($path) data-path="{{$path ?? 'Y/m/d'}}" @endif
+        @if ($configExt) data-config-ext="{{ $configExt}}" @endif
+    >
         <div class="tinymce border p-3" id="tinymce-wrapper-{{$id}}" style="min-height: {{ $attributes['height'] }}">
             {!! $value !!}
         </div>
